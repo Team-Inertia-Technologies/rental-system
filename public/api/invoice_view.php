fc6c7dbe2b29859f83dfdc62d57829185a11eea2
Myron <myron@teaminertia.com>
2026-01-07 16:52:30 +0530

    modified
    Signed-off-by: Myron <myron@teaminertia.com>

---
+++ public/api/invoice_view.php
@@ -0,0 +1,69 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 0);
+$NO_REDIRECT = $NO_PRELOAD = 1;
+include "../includes/common_api.php";
+
+header('Content-Type: application/json');
+$postdata = file_get_contents("php://input");
+
+$request = json_decode($postdata, true);
+$_REQUEST = array_merge($_REQUEST, $request ?? []);
+
+$token = $_REQUEST['token'] ?? '';
+$invoiceId = isset($_REQUEST['invoiceId']) ? (int)$_REQUEST['invoiceId'] : 0;
+
+try {
+	$query = " SELECT 
+		i.iInvoiceID AS id,
+		i.vInvoiceNo AS invoiceNo,
+		i.dInvoiceDate AS invoiceDate,
+		i.fTotal AS totalAmount,
+		i.cStatus AS status,
+		p.vName AS propertyName,
+		t.vParty AS tenantName,
+		i.tDetails AS details
+	FROM invoice i
+	JOIN property p ON i.iPropertyID = p.iPropertyID
+	JOIN tenant t ON i.iTenantID = t.iTenantID
+	WHERE i.iInvoiceID = $invoiceId
+	";
+	$res = sql_query($query);
+	if ($data = sql_fetch_assoc($res)) {
+		$invoice = array(
+			"invoiceId" => (int)$data['id'],
+			"invoiceNumber" => $data['invoiceNo'],
+			"date" => $data['invoiceDate'],
+			"total" => (float)$data['totalAmount'],
+			"status" => $data['status'],
+			"statusText"=> 'Paid',
+			"propertyName" => $data['propertyName'],
+			"tenantName" => $data['tenantName'],
+			"details" => $data['details'],
+		);
+	} else {
+		throw new Exception("Invoice not found");
+	}
+
+	$response = array(
+		"data" => array(
+			"invoice" => $invoice,
+		),
+		"statusCode" => 200,
+	);
+	http_response_code(200);
+	echo json_encode($response);
+	exit;
+
+} catch (Exception $e) {
+	$response = array(
+		"error" => array(
+			"message" => "Internal Server Error",
+		),
+		"statusCode" => 500,
+	);
+	http_response_code(500);
+	echo json_encode($response);
+	exit;
+}
+?>
