1e9ed03fb40ef2d76a9918dd1d9355f45e848a1a
Myron <myron@teaminertia.com>
2026-01-09 10:45:13 +0530

    modified
    Signed-off-by: Myron <myron@teaminertia.com>

--- public/api/addproperty_onload.php
+++ public/api/addproperty_onload.php
@@ -11,51 +11,165 @@ $request = json_decode($postdata, true);
 $_REQUEST = array_merge($_REQUEST, $request ?? []);
 
 $token = $_REQUEST['token'] ?? '';
+$mode   = strtoupper(trim($_REQUEST['mode'] ?? ''));
+$propId = isset($_REQUEST['propId']) ? (int)$_REQUEST['propId'] : 0;
+
+if (empty($token)) {
+    http_response_code(404);
+    echo json_encode([
+        "statusCode" => 404,
+        "message" => "Token missing"
+    ]);
+    exit;
+}
+
+if (!in_array($mode, ['INSERT', 'UPDATE'])) {
+    http_response_code(400);
+    echo json_encode(["statusCode" => 400, "message" => "Invalid mode"]);
+    exit;
+}
 
 try{
 
-	$categoryquery = "SELECT iCategoryID, vName FROM category WHERE cStatus='A' ORDER BY vName ASC";
-	$categoryres = sql_query($categoryquery);
-	$categories = [];
-	while ($data = sql_fetch_assoc($categoryres)) {
-		$categories[] = array(
-			"id" => (int)$data['iCategoryID'],
-			"catName" => $data['vName'],
+	if ($mode === 'INSERT') {
+
+		$categoryquery = "SELECT iCategoryID, vName FROM category WHERE cStatus='A' ORDER BY vName ASC";
+		$categoryres = sql_query($categoryquery);
+		$categories = [];
+		while ($data = sql_fetch_assoc($categoryres)) {
+			$categories[] = array(
+				"id" => (int)$data['iCategoryID'],
+				"catName" => $data['vName'],
+			);
+		}
+
+		$tenantquery = "SELECT iTenantID, vParty FROM tenant WHERE cStatus='A' ORDER BY vParty ASC";
+		$tenantres = sql_query($tenantquery);
+		$tenants = [];
+		while ($data = sql_fetch_assoc($tenantres)) {
+			$tenants[] = array(
+				"id" => (int)$data['iTenantID'],
+				"tenantName" => $data['vParty'],
+			);
+		}
+
+		$propertytypequery = "SELECT iPropertyTypeID, vName FROM property_type WHERE cStatus='A' ORDER BY vName ASC";
+		$propertytyperes = sql_query($propertytypequery);
+		$propertytypes = [];
+		while ($data = sql_fetch_assoc($propertytyperes)) {
+			$propertytypes[] = array(
+				"id" => (int)$data['iPropertyTypeID'],
+				"typeName" => $data['vName'],
+			);
+		}
+
+		$response = array(
+			"data" => array(
+				"categoryArr" => $categories,
+				"tenantArr" => $tenants,
+				"propertyTypeArr" => $propertytypes,
+			),
+			"statusCode" => 200,
 		);
+	
+		http_response_code(200);
+		echo json_encode($response);
+		exit;
 	}
 
-	$tenantquery = "SELECT iTenantID, vParty FROM tenant WHERE cStatus='A' ORDER BY vParty ASC";
-	$tenantres = sql_query($tenantquery);
-	$tenants = [];
-	while ($data = sql_fetch_assoc($tenantres)) {
-		$tenants[] = array(
-			"id" => (int)$data['iTenantID'],
-			"tenantName" => $data['vParty'],
+	if ($mode === 'UPDATE') {
+
+		$categoryquery = "SELECT iCategoryID, vName FROM category WHERE cStatus='A' ORDER BY vName ASC";
+		$categoryres = sql_query($categoryquery);
+		$categories = [];
+		while ($data = sql_fetch_assoc($categoryres)) {
+			$categories[] = array(
+				"id" => (int)$data['iCategoryID'],
+				"catName" => $data['vName'],
+			);
+		}
+
+		$tenantquery = "SELECT iTenantID, vParty FROM tenant WHERE cStatus='A' ORDER BY vParty ASC";
+		$tenantres = sql_query($tenantquery);
+		$tenants = [];
+		while ($data = sql_fetch_assoc($tenantres)) {
+			$tenants[] = array(
+				"id" => (int)$data['iTenantID'],
+				"tenantName" => $data['vParty'],
+			);
+		}
+
+		$propertytypequery = "SELECT iPropertyTypeID, vName FROM property_type WHERE cStatus='A' ORDER BY vName ASC";
+		$propertytyperes = sql_query($propertytypequery);
+		$propertytypes = [];
+		while ($data = sql_fetch_assoc($propertytyperes)) {
+			$propertytypes[] = array(
+				"id" => (int)$data['iPropertyTypeID'],
+				"typeName" => $data['vName'],
+			);
+		}
+
+		if ($propId <= 0) {
+			http_response_code(400);
+			echo json_encode([
+				"statusCode" => 400,
+				"message" => "Invalid property ID"
+			]);
+			exit;
+		}
+
+		$propertyQuery = " SELECT 
+			p.iPropertyID AS id,
+			p.vName AS propertyName,
+			p.vAddress AS address,
+			c.iCategoryID AS categoryId,
+			pt.iPropertyTypeID AS typeId,
+			p.fCarpetArea AS carpetArea,
+			p.fBuiltArea AS builtUpArea,
+			a.dFrom,
+			a.dTo,
+			a.fAmount,
+			a.vAgreementDoc,
+			a.iTenantID,
+			t.vParty
+		FROM property p
+		JOIN category c ON p.iCategoryID = c.iCategoryID
+		JOIN property_type pt ON p.iPropertyTypeID = pt.iPropertyTypeID
+		LEFT JOIN agreement a ON a.iPropertyID = p.iPropertyID
+		LEFT JOIN tenant t ON t.iTenantID = a.iTenantID
+		WHERE p.iPropertyID = $propId
+		";
+		$propertyRes = sql_query($propertyQuery);
+		$propertyData = sql_fetch_assoc($propertyRes);
+		
+		$response = array(
+			"data" => array(
+				"propertyDetails" => array(
+					"propId" => (int)$propertyData['id'],
+					"name" => $propertyData['propertyName'],
+					"address" => $propertyData['address'],
+					"category" => (int)$propertyData['categoryId'],
+					"type" => (int)$propertyData['typeId'],
+					"carpetArea" => (float)$propertyData['carpetArea'],
+					"builtUpArea" => (float)$propertyData['builtUpArea'],
+					"tenantId" => (int)$propertyData['iTenantID'],
+					"tenantName" => $propertyData['vParty'],
+					"monthlyRent" => (float)$propertyData['fAmount'],
+					"leaseStart" => $propertyData['dFrom'],
+					"leaseEnd" => $propertyData['dTo'],
+					"agreementdoc" => $propertyData['vAgreementDoc'],
+				),
+				"categoryArr" => $categories,
+				"tenantArr" => $tenants,
+				"propertyTypeArr" => $propertytypes,
+			),
+			"statusCode" => 200,
 		);
+
+		http_response_code(200);
+		echo json_encode($response);
+		exit;
 	}
-
-	$propertytypequery = "SELECT iPropertyTypeID, vName FROM property_type WHERE cStatus='A' ORDER BY vName ASC";
-	$propertytyperes = sql_query($propertytypequery);
-	$propertytypes = [];
-	while ($data = sql_fetch_assoc($propertytyperes)) {
-		$propertytypes[] = array(
-			"id" => (int)$data['iPropertyTypeID'],
-			"typeName" => $data['vName'],
-		);
-	}
-
-	$response = array(
-		"data" => array(
-			"categoryArr" => $categories,
-			"tenantArr" => $tenants,
-			"propertyTypeArr" => $propertytypes,
-		),
-		"statusCode" => 200,
-	);
-
-	http_response_code(200);
-	echo json_encode($response);
-	exit;
 
 } catch (Exception $e) {
 	$response = array(
