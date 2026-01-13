7f8c8f6904731a27b2280c24724197334cd8d5d1
Myron <myron@teaminertia.com>
2026-01-13 14:41:04 +0530

    logout api
    Signed-off-by: Myron <myron@teaminertia.com>

---
+++ public/api/logout.php
@@ -0,0 +1,17 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 0);
+include "../includes/common_api.php";
+$NO_REDIRECT = $NO_PRELOAD = 1;
+header ('Content-Type: application/json');
+$token = isset($_POST['token']) ? db_input($_POST['token']) : '';
+$user_id = DecodeParam($token);
+sql_query("UPDATE users SET cActive='N' WHERE iUserID=$user_id");
+
+session_destroy();
+$response = array('statusCode' => 200, 'message' => "Logged out successfully");
+http_response_code(200);
+header('Content-Type: application/json');
+echo json_encode($response);
+exit;
+?>
